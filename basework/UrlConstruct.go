64f3e3332e14ffd9ae93e79fd31d215579d85c4d
Yim1ngs <3305672433@qq.com>
2025-12-03 22:47:45 +0800

    优化url输入

--- basework/UrlConstruct.go
+++ basework/UrlConstruct.go
@@ -2,21 +2,34 @@ package basework
 
 import (
 	"bufio"
+	"net/url"
 	"os"
+	"strings"
 )
 
 func UrlConstruct(urlStr string, filename string) (<-chan string, error) {
 	out := make(chan string, 100) // 缓冲通道
+
+	url1, err := url.Parse(urlStr)
+	if err != nil {
+		return nil, err
+	}
+
+	if url1.Scheme == "" {
+		url1.Scheme = "http"
+	}
+
+	if !strings.HasPrefix(url1.Path, "/") {
+		url1.Path = url1.Path + "/"
+	}
+
+	urlStr = url1.String()
+
 	file, err := os.Open(filename)
 	if err != nil {
 		return nil, err
 	}
 
-	//base, err := url.Parse(urlStr)
-	//if err != nil{
-	//	log.Printf("error:%v",err)
-	//	return nil
-	//}
 	go func() {
 		defer file.Close()
 		defer close(out)
